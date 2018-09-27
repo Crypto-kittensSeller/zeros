ab50ed4d9ea5434808f92ee66b96976ff5dc9a67
Crypto-kittensSeller <Nikolay.Shaveiko@gmail.com>
2018-09-27 13:18:04 +0300

    zeros counter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let zerocounter = 0;
+  for (let i = 1; i <= number; i++){
+    let x = i;
+    while (x % 5 === 0){
+      x /= 5;
+      zerocounter++;                  
+    }
+  }
+  return zerocounter;
 }
