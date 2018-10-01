335bc740be9a9aee216d340049810af5dfe7158d
Crypto-kittensSeller <Nikolay.Shaveiko@gmail.com>
2018-10-01 12:52:05 +0300

    performance improvement

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 module.exports = function getZerosCount(number) {
   let zerocounter = 0;
-  for (let i = 1; i <= number; i++){
-    let x = i;
-    while (x % 5 === 0){
-      x /= 5;
-      zerocounter++;                  
-    }
+  let x = number;
+  
+  while (x >= 5){
+  zerocounter +=Math.floor(x / 5);
+  x /= 5;
   }
   return zerocounter;
 }
